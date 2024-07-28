ae1b530404c239dbf1ec26456cd4d080c12670d3
esteban <ejgr1990ar@gmail.com>
2024-07-27 22:01:03 -0300

    Update-css responsivo

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import { applyTheme } from './theme.js';
-import { challange } from './challange.js';
+import { variables } from './variables.js';
 
 
+/* Evento que cambia el tema */
 document.addEventListener('DOMContentLoaded', function() {
     const themeSelect = document.getElementById('theme-select');
     themeSelect.addEventListener('change', function() {
@@ -9,3 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
         applyTheme(theme);
     });
 });
+
+
+/* Funciones de los botones */
+
+/* Listeners  de variables.js*/
+variables.LowerCase()
+variables.readOnly()
+variables.copiar()
+variables.desencriptar()
+variables.encriptar()
---
+++ src/js/variables.js
@@ -0,0 +1,78 @@
+import { challange } from './challange.js';
+import { regex} from './regexp.js';
+
+class Variables{
+    constructor(){
+        this.btnDesencriptar =  document.getElementById('btn-encriptar')
+        this.btnEncriptar = document.getElementById('btn-desencriptar')
+        this.btnCopiar = document.getElementById('btn-copiar')
+        this.txtareaDesencriptado = document.getElementById('txtarea-desencriptado')
+        this.txtareaEncriptado = document.getElementById('txtarea-encriptado')
+    }
+
+    LowerCase(){
+        this.txtareaDesencriptado.addEventListener('input', () => {
+            const texto = this.txtareaDesencriptado.value;
+            const textoLowerCase = texto.toLowerCase();
+            if (texto !== textoLowerCase) {
+                this.txtareaDesencriptado.value = textoLowerCase;
+            }
+        });
+    }
+
+    copiar(){
+        this.btnCopiar.addEventListener('click', ()=>{
+            const textoACopiar = this.txtareaEncriptado.value;
+            navigator.clipboard.writeText(textoACopiar).then(()=>{
+            }).catch(error =>{
+                console.log(`Error al copiar el texto: ${error}`);
+            })
+        })
+    }
+
+    encriptar(){
+        this.btnEncriptar.addEventListener('click', ()=>{
+
+            if ( this.verificar() ){
+                console.log('hay tildes')
+            }else{
+                challange.setTexto(this.txtareaDesencriptado.value)
+                this.txtareaEncriptado.value = challange.encriptar()
+            }
+        })
+    }
+
+    desencriptar(){
+        this.btnDesencriptar.addEventListener('click', ()=>{
+            if ( this.verificar() ){
+                console.log('hay tildes')
+            }else{
+                challange.setTexto(this.txtareaDesencriptado.value)
+                let texto = challange.getTexto()
+                this.txtareaEncriptado.value = challange.desencriptarv2(texto)
+            }
+        })
+    }
+
+    verificar(){
+        let textoVerificar = this.txtareaDesencriptado.value
+        return regex.test(textoVerificar)
+    }
+
+    readOnly() {
+        // Agrega el atributo readonly al textarea
+        this.txtareaEncriptado.setAttribute('readonly', true);
+
+        // Prevenir modificaciones mediante eventos de teclado
+        this.txtareaEncriptado.addEventListener('keydown', (e) => {
+            e.preventDefault();
+        });
+
+        // Prevenir modificaciones mediante eventos de entrada (input)
+        this.txtareaEncriptado.addEventListener('input', (e) => {
+            e.preventDefault();
+        });
+    }
+}
+
+export const variables = new Variables()
